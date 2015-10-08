01b4cecd412ff9743fd882f4567be7ef352fb35f
Nolan Baker <nolan.lee.baker@gmail.com>
2015-10-07 22:09:35 -0500

    adds player number, static event on change, code style update

--- Scripts/Control/InputControl.cs
+++ Scripts/Control/InputControl.cs
@@ -1,176 +1,151 @@
 using System;
 using UnityEngine;
 
+namespace InControl {
+public class InputControl {
 
-namespace InControl
-{
-	public class InputControl
-	{
-		public static readonly InputControl Null = new InputControl( "NullInputControl" );
+	public delegate void OnInputChanged(InputControl inputControl);
+	public static event OnInputChanged onInputChanged = delegate {};
 
-		public string Handle { get; private set; }
-		public InputControlType Target { get; private set; }
+	public int player = -1;
+	public static readonly InputControl Null = new InputControl( "NullInputControl" );
 
-		public ulong UpdateTick { get; private set; }
+	public string Handle { get; private set; }
+	public InputControlType Target { get; private set; }
 
-		public float Sensitivity = 1.0f;
-		public float LowerDeadZone = 0.0f;
-		public float UpperDeadZone = 1.0f;
+	public ulong UpdateTick { get; private set; }
 
-		InputControlState thisState;
-		InputControlState lastState;
-		InputControlState tempState;
+	public float Sensitivity = 1.0f;
+	public float LowerDeadZone = 0.0f;
+	public float UpperDeadZone = 1.0f;
+
+	InputControlState thisState;
+	InputControlState lastState;
+	InputControlState tempState;
 
 
-		private InputControl( string handle )
-		{
-			Handle = handle;
+	private InputControl( string handle ) {
+		Handle = handle;
+	}
+
+
+	public InputControl( string handle, InputControlType target ) {
+		Handle = handle;
+		Target = target;
+	}
+
+
+	public void UpdateWithState( bool state, ulong updateTick ) {
+		if (IsNull) {
+			throw new InvalidOperationException( "A null control cannot be updated." );
 		}
 
-
-		public InputControl( string handle, InputControlType target )
-		{
-			Handle = handle;
-			Target = target;
+		if (UpdateTick > updateTick) {
+			throw new InvalidOperationException( "A control cannot be updated with an earlier tick." );
 		}
 
+		tempState.Set( state || tempState.State );
+	}
 
-		public void UpdateWithState( bool state, ulong updateTick )
-		{
-			if (IsNull)
-			{
-				throw new InvalidOperationException( "A null control cannot be updated." );
-			}
 
-			if (UpdateTick > updateTick)
-			{
-				throw new InvalidOperationException( "A control cannot be updated with an earlier tick." );
-			}
-
-			tempState.Set( state || tempState.State );
+	public void UpdateWithValue( float value, ulong updateTick ) {
+		if (IsNull) {
+			throw new InvalidOperationException( "A null control cannot be updated." );
 		}
 
-
-		public void UpdateWithValue( float value, ulong updateTick )
-		{
-			if (IsNull)
-			{
-				throw new InvalidOperationException( "A null control cannot be updated." );
-			}
-
-			if (UpdateTick > updateTick)
-			{
-				throw new InvalidOperationException( "A control cannot be updated with an earlier tick." );
-			}
-
-			if (Mathf.Abs( value ) > Mathf.Abs( tempState.Value ))
-			{
-				tempState.Set( value );
-			}
+		if (UpdateTick > updateTick) {
+			throw new InvalidOperationException( "A control cannot be updated with an earlier tick." );
 		}
 
-
-		internal void PreUpdate( ulong updateTick )
-		{
-			lastState = thisState;
-			tempState.Reset();
+		if (Mathf.Abs( value ) > Mathf.Abs( tempState.Value )) {
+			tempState.Set( value );
 		}
+	}
 
 
-		internal void PostUpdate( ulong updateTick )
-		{
-			thisState = tempState;
-			if (thisState != lastState)
-			{
-				UpdateTick = updateTick;
-			}
+	internal void PreUpdate( ulong updateTick ) {
+		lastState = thisState;
+		tempState.Reset();
+	}
+
+
+	internal void PostUpdate( ulong updateTick ) {
+		thisState = tempState;
+		if (thisState != lastState) {
+			onInputChanged(this);
+			UpdateTick = updateTick;
 		}
+	}
 
 
-		public bool State
-		{
-			get { return thisState.State; }
-		}
+	public bool State {
+		get { return thisState.State; }
+	}
 
 
-		public bool LastState
-		{
-			get { return lastState.State; }
-		}
+	public bool LastState {
+		get { return lastState.State; }
+	}
 
 
-		public float Value
-		{
-			get { return thisState.Value; }
-		}
+	public float Value {
+		get { return thisState.Value; }
+	}
 
 
-		public float LastValue
-		{
-			get { return lastState.Value; }
-		}
+	public float LastValue {
+		get { return lastState.Value; }
+	}
 
 
-		public bool HasChanged
-		{
-			get { return thisState != lastState; }
-		}
+	public bool HasChanged {
+		get { return thisState != lastState; }
+	}
 
 
-		public bool IsPressed
-		{
-			get { return thisState.State; }
-		}
+	public bool IsPressed {
+		get { return thisState.State; }
+	}
 
 
-		public bool WasPressed
-		{
-			get { return thisState && !lastState; }
-		}
+	public bool WasPressed {
+		get { return thisState && !lastState; }
+	}
 
 
-		public bool WasReleased
-		{
-			get { return !thisState && lastState; }
-		}
+	public bool WasReleased {
+		get { return !thisState && lastState; }
+	}
 
 
-		public bool IsNull
-		{
-			get { return this == Null; }
-		}
+	public bool IsNull {
+		get { return this == Null; }
+	}
 
 
-		public bool IsNotNull
-		{
-			get { return this != Null; }
-		}
+	public bool IsNotNull {
+		get { return this != Null; }
+	}
 
 
-		public override string ToString()
-		{
-			return string.Format( "[InputControl: Handle={0}, Value={1}]", Handle, Value );
-		}
+	public override string ToString() {
+		return string.Format( "[InputControl: Handle={0}, Value={1}]", Handle, Value );
+	}
 
 
-		public static implicit operator bool( InputControl control )
-		{
-			return control.State;
-		}
+	public static implicit operator bool( InputControl control ) {
+		return control.State;
+	}
 
 
-		public static implicit operator float( InputControl control )
-		{
-			return control.Value;
-		}
+	public static implicit operator float( InputControl control ) {
+		return control.Value;
+	}
 
 
-		public InputControlType? Obverse
-		{
-			get
-			{
-				switch (Target)
-				{
+	public InputControlType? Obverse {
+		get {
+			switch (Target) {
 				case InputControlType.LeftStickX:
 					return InputControlType.LeftStickY;
 				case InputControlType.LeftStickY:
@@ -181,13 +156,13 @@ namespace InControl
 					return InputControlType.RightStickX;
 				default:
 					return null;
-				}				
-			}
+			}				
 		}
-
-
-		// This is for internal use only and is not always set.
-		internal float? RawValue;
-		internal float? PreValue;
 	}
+
+
+	// This is for internal use only and is not always set.
+	internal float? RawValue;
+	internal float? PreValue;
+}
 }
